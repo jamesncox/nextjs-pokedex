ee272a24c577584b049ff8f078d06e76ff2d33cc
jamesncox <jamesncox@yahoo.com>
2021-03-12 10:03:19 -0600

    Update index.js with async function getStaticProps()

--- pages/index.js
+++ pages/index.js
@@ -9,3 +9,24 @@ export default function Home() {
     </Layout>
   )
 }
+
+export async function getStaticProps(context) {
+  try {
+    const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=150')
+    const {results} = await res.json()
+    const pokemon = results.map((result, index) => {
+      const paddedId = ('00' + (index + 1)).slice(-3);
+      const image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+      return {
+        ...result,
+        image
+      }
+    })
+  } catch (err) {
+    console.error(err)
+  }
+
+  return {
+    props: {}
+  }
+}
