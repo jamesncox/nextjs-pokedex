79a1d78d7592b62150024f8e6c1393a88d2b3a4a
jamesncox <jamesncox@yahoo.com>
2021-03-12 09:54:33 -0600

    Update pages/index.js h1 add tailwind classes

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout title="NextJS Pokedex">
-        <h1>
+        <h1 className="text-4xl mb-8 text-center">
           NextJS Pokedex
         </h1>
     </Layout>
