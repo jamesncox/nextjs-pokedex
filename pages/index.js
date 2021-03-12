bd7f52af3f3880939e1d7a38f3fd17064e810a85
jamesncox <jamesncox@yahoo.com>
2021-03-12 09:19:39 -0600

    Update <h1> NextJS Pokedex

--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,8 @@ import Layout from '../components/Layout'
 export default function Home() {
   return (
     <Layout title="NextJS Pokedex">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
+        <h1>
+          NextJS Pokedex
         </h1>
     </Layout>
   )
