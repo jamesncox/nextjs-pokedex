fdf7f13abe6f4fe466dcf029bc889ee963e878b2
jamesncox <jamesncox@yahoo.com>
2021-03-12 09:15:16 -0600

    Update Layout.js with {title, children} and some tailwind

--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import Head from 'next/head'
 
-export default function Layout() {
+export default function Layout({title, children}) {
 
     return (
-        <div></div>
+        <div className="bg-gray-300">
+            <Head>
+                <title>{title}</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className="container mx-auto max-w-xl pt-8 min-h-screen">
+                {children}
+            </main>
+        </div>
     )
 }
