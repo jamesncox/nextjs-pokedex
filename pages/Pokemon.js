0f66cc18bbe02cddda919a173f4a6836bb91e830
jamesncox <jamesncox@yahoo.com>
2021-03-12 10:32:59 -0600

    Add getServerSideProps to Pokemon.js

--- pages/Pokemon.js
+++ pages/Pokemon.js
@@ -1,11 +1,27 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import Link from 'next/link'
 
-export default function Pokemon() {
+export default function Pokemon({pokeman}) {
 
     return (
         <div>
 
         </div>
     )
+}
+
+export async function getServerSideProps({ query }) {
+    const id = query.id;
+    try {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const pokeman = await res.json();
+        const paddedId = ('00' + id).slice(-3);
+        pokeman.image = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${paddedId}.png`;
+        return {
+            props: { pokeman },
+        };
+    } catch (err) {
+        console.error(err);
+    }
 }
